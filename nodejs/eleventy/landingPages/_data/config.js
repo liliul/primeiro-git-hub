330efd8459f3ebfd023e1d674111840620248d5d
liliul <liliuestudo@gmail.com>
2025-07-06 02:01:44 -0300

    update: header com dados dinamicos config js landingPages eleventy

--- nodejs/eleventy/landingPages/_data/config.js
+++ nodejs/eleventy/landingPages/_data/config.js
@@ -1,4 +1,21 @@
 module.exports = {
     simonao: true,
-    nome: "Liliul"
+    nome: "Liliul",
+	bio: `
+        Meu nome é Liliul e sou desenvolvedor Full Stack. 
+        Crio aplicações web completas utilizando JavaScript, Node.js,
+        MySQL e Docker, com foco em desempenho e organização. 
+        Confira meus projetos ou entre em contato para colaborarmos!
+    `,
+    skillsTech: [
+	'Javascript',
+	'html/css/sass',
+	'node',
+	'typescript',
+	'php',
+	'react',
+	'firebase',
+	'git/github',
+	'docker/compose'
+    ]
 };
