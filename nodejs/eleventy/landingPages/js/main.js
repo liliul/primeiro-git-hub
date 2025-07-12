b145d25cd15d8fc25b440a23323c8d543d323088
liliul <liliuestudo@gmail.com>
2025-07-12 18:38:27 -0300

    feat: tema com cores dinamica landingPages eleventy

--- nodejs/eleventy/landingPages/js/main.js
+++ nodejs/eleventy/landingPages/js/main.js
@@ -70,7 +70,11 @@ class AlertCustom {
         div.classList.add('alert-custom');
 
         div.innerHTML = `
-            <button id="close-alert-custom">x</button>
+            <button id="close-alert-custom">
+              <svg viewBox="0 0 24 24" class="fechar-svg" fill="none" xmlns="http://www.w3.org/2000/svg"><g stroke-width="0"></g>
+                <g stroke-linecap="round" stroke-linejoin="round"></g><g> 
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#DDD"></path> </g></svg>
+            </button>
             <h1>${menssagem}</h1>
         `;
 
@@ -89,3 +93,19 @@ class AlertCustom {
 }
 
 CopyEmail.copy()
+
+
+const textoCor = "#fa7f72";
+const cardHoverCor = "#E9967A";
+const txtPskills1 = "#111111";
+
+
+let txt        = "--bg-techs";
+let lang       = "--bg-lang";
+let cardHover  = "--card-hover-cor";
+let txtPskills = "--text-p-skills";
+
+document.documentElement.style.setProperty(txt, textoCor);
+document.documentElement.style.setProperty(lang, textoCor);
+document.documentElement.style.setProperty(cardHover, cardHoverCor);
+document.documentElement.style.setProperty(txtPskills, txtPskills1);
