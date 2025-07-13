b4089c85b3df796fef0d4a8e0077d69aed2cd8bf
liliul <liliuestudo@gmail.com>
2025-07-12 21:43:03 -0300

    add: type module javascript landingPages eleventy

---
+++ nodejs/eleventy/landingPages/js/cor.js
@@ -0,0 +1,17 @@
+export function Cor() {
+    const textoCor = "#fa7f72";
+const cardHoverCor = "#E9967A";
+const txtPskills1 = "#111111";
+
+
+let txt        = "--bg-techs";
+let lang       = "--bg-lang";
+let cardHover  = "--card-hover-cor";
+let txtPskills = "--text-p-skills";
+
+document.documentElement.style.setProperty(txt, textoCor);
+document.documentElement.style.setProperty(lang, textoCor);
+document.documentElement.style.setProperty(cardHover, cardHoverCor);
+document.documentElement.style.setProperty(txtPskills, txtPskills1);
+
+}
--- nodejs/eleventy/landingPages/js/main.js
+++ nodejs/eleventy/landingPages/js/main.js
@@ -1,3 +1,6 @@
+import { Cor } from "./cor.js";
+Cor()
+
 document.addEventListener('DOMContentLoaded', function () {
   console.log('O script foi carregado com sucesso!');
 
@@ -95,17 +98,17 @@ class AlertCustom {
 CopyEmail.copy()
 
 
-const textoCor = "#fa7f72";
-const cardHoverCor = "#E9967A";
-const txtPskills1 = "#111111";
+// const textoCor = "#fa7f72";
+// const cardHoverCor = "#E9967A";
+// const txtPskills1 = "#111111";
 
 
-let txt        = "--bg-techs";
-let lang       = "--bg-lang";
-let cardHover  = "--card-hover-cor";
-let txtPskills = "--text-p-skills";
+// let txt        = "--bg-techs";
+// let lang       = "--bg-lang";
+// let cardHover  = "--card-hover-cor";
+// let txtPskills = "--text-p-skills";
 
-document.documentElement.style.setProperty(txt, textoCor);
-document.documentElement.style.setProperty(lang, textoCor);
-document.documentElement.style.setProperty(cardHover, cardHoverCor);
-document.documentElement.style.setProperty(txtPskills, txtPskills1);
+// document.documentElement.style.setProperty(txt, textoCor);
+// document.documentElement.style.setProperty(lang, textoCor);
+// document.documentElement.style.setProperty(cardHover, cardHoverCor);
+// document.documentElement.style.setProperty(txtPskills, txtPskills1);
