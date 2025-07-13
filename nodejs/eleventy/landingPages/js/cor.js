b96c2b2fcc75f4b240ed21a990600e555146b08a
liliul <liliuestudo@gmail.com>
2025-07-13 01:49:32 -0300

    chore: organizando arquivo landingPages eleventy

--- nodejs/eleventy/landingPages/js/cor.js
+++ nodejs/eleventy/landingPages/js/cor.js
@@ -1,17 +1,16 @@
 export function Cor() {
     const textoCor = "#fa7f72";
-const cardHoverCor = "#E9967A";
-const txtPskills1 = "#111111";
+    const cardHoverCor = "#E9967A";
+    const txtPskills1 = "#111111";
 
 
-let txt        = "--bg-techs";
-let lang       = "--bg-lang";
-let cardHover  = "--card-hover-cor";
-let txtPskills = "--text-p-skills";
+    let txt        = "--bg-techs";
+    let lang       = "--bg-lang";
+    let cardHover  = "--card-hover-cor";
+    let txtPskills = "--text-p-skills";
 
-document.documentElement.style.setProperty(txt, textoCor);
-document.documentElement.style.setProperty(lang, textoCor);
-document.documentElement.style.setProperty(cardHover, cardHoverCor);
-document.documentElement.style.setProperty(txtPskills, txtPskills1);
-
+    document.documentElement.style.setProperty(txt, textoCor);
+    document.documentElement.style.setProperty(lang, textoCor);
+    document.documentElement.style.setProperty(cardHover, cardHoverCor);
+    document.documentElement.style.setProperty(txtPskills, txtPskills1);
 }
