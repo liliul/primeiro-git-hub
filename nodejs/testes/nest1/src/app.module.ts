f3e3b0e3f61a48e125536edd663aec6a995bbf49
liliul <liliuestudo@gmail.com>
2025-08-24 11:11:00 -0300

    chore: limpando projeto nest1 testes

--- nodejs/testes/nest1/src/app.module.ts
+++ nodejs/testes/nest1/src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CatsModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
