7a6e5ee0c6ce7ce8333540069f5c4772ec94dd31
liliul <liliuestudo@gmail.com>
2025-08-30 00:23:24 -0300

    add: auth nas rotas anime sqlite modulosNodejs

--- nodejs/modulosNodejs/sqlite/src/expressPerson/routers/animeRouter.js
+++ nodejs/modulosNodejs/sqlite/src/expressPerson/routers/animeRouter.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const db = require('../database')
 const AnimeController = require('../controllers/animeController')
+const authenticateToken = require('../middleware/auth')
+const authorizeRoles = require('../middleware/authRoles')
 
 const routerAnime = express.Router()
 
 const animeController = new AnimeController(db)
 
-routerAnime.post('/create-anime', animeController.create.bind(animeController))
+routerAnime.post('/create-anime', authenticateToken, authorizeRoles('admin', 'master'), animeController.create.bind(animeController))
 routerAnime.get('/search', animeController.search.bind(animeController))
 routerAnime.get('/search/:id', animeController.searchID.bind(animeController))
-routerAnime.delete('/delete-anime/:id', animeController.delete.bind(animeController))
-routerAnime.put('/update-anime/:id', animeController.update.bind(animeController))
+routerAnime.delete('/delete-anime/:id', authenticateToken, authorizeRoles('admin', 'master'), animeController.delete.bind(animeController))
+routerAnime.put('/update-anime/:id', authenticateToken, authorizeRoles('admin', 'master'), animeController.update.bind(animeController))
 
 module.exports = routerAnime
