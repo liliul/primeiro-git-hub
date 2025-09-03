89622944d7118f7dcbad34395c669f89585e1405
liliul <liliuestudo@gmail.com>
2025-09-02 22:03:23 -0300

    chore: readme index readme youtubeData

--- nodejs/testes/youtubeData/README.md
+++ nodejs/testes/youtubeData/README.md
@@ -4,3 +4,29 @@
 
 YOUTUBE_API_KEY=
 ```
+
+docker postgres
+
+```bash
+
+# criar container
+docker run --name meu_postgres \
+  -e POSTGRES_USER=root \
+  -e POSTGRES_PASSWORD=admin \
+  -e POSTGRES_DB=youtubedata \
+  -p 5432:5432 \
+  -d postgres
+
+docker exec -it meu_postgres psql -U root -d youtubedata
+
+# ver tabelas
+
+\dt
+
+\d youtube_videos
+
+# sair
+
+\q
+
+```
--- nodejs/testes/youtubeData/src/index.js
+++ nodejs/testes/youtubeData/src/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import routerHealth from './router/health.js';
 import routerAccount from './router/routerAccount.js';
+import routerYoutubeAlta from './router/routerYoutubeAlta.js';
 
 const app = express()
 app.use(express.json());
 
 app.use(routerHealth)
 app.use(routerAccount)
+app.use('/youtube/v1', routerYoutubeAlta)
 
 app.listen(3001, () => {
   console.log('Servidor rodando em http://localhost:3001');
