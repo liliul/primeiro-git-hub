39e6ab7fde7834002d51fcc728a6f2fbe881134c
liliul <liliuestudo@gmail.com>
2025-09-10 15:41:18 -0300

    create: middleware jwt carrinhoCart

---
+++ nodejs/testes/carrinhoCart/src/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+dotenv.config();
+
+function AuthorizationJwt (req, res, next) {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(401).json({ error: 'Token não fornecido' });
+  
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.error('Erro na verificação do token:', err.message);
+    res.status(401).json({ error: 'Token inválido' });
+  }
+};
+
+export default AuthorizationJwt
---
+++ nodejs/testes/carrinhoCart/src/middleware/roles.js
@@ -0,0 +1,12 @@
+function AuthRoles (roles = []) {
+  if (typeof roles === 'string') roles = [roles]
+
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ error: 'Acesso negado' })
+    }
+    next()
+  }
+}
+
+export default AuthRoles
