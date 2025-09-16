03f6af681c3f7a6c59d762f1b152d31ca0edbbb5
liliul <liliuestudo@gmail.com>
2025-09-15 23:29:49 -0300

    feat: update isAuthenticated carrinhoCart

--- nodejs/testes/carrinhoCart/src/middleware/roles.js
+++ nodejs/testes/carrinhoCart/src/middleware/roles.js
@@ -1,3 +1,5 @@
+import db from '../db/indexDB.js'
+
 const isSuperAdmin = (req, res, next) => {
     if (req.user && req.user.role === 'super-admin') {
         next()
@@ -14,16 +16,45 @@ const isAdmin = (req, res, next) => {
     }
 }
 
-const isAuthenticated = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.status(401).json({ message: 'Não autorizado. Faça login para continuar.' });
+const isAuthenticated =  async (req, res, next) => {
+    const userIdFromToken = req.user.id;
+    const userRole = req.user.role;
+    const targetUserId = req.params.id;
+
+    try {
+      if (userRole === 'super-admin') {
+        return next()
+      }
+
+      if (userIdFromToken === targetUserId) {
+        return next();
+      }
+      
+      const targetUserResult = await db.query('SELECT role FROM users WHERE id = $1', [targetUserId]);
+      
+      if (targetUserResult.rowCount === 0) {
+          return next();
+      }
+
+      const targetUserRole = targetUserResult.rows[0].role;
+
+      if (userRole === 'user' && (targetUserRole === 'admin' || targetUserRole === 'super-admin')) {
+          return res.status(403).json({ 
+              message: 'Acesso negado. Você não pode editar um perfil de administrador.' 
+          });
+      }
+      
+      res.status(403).json({ 
+          message: 'Acesso negado. Você não tem permissão para editar as informações deste usuário.' 
+      })
+    } catch (error) {
+        console.error('Erro na autorização:', error);
+        res.status(500).json({ message: 'Erro interno do servidor.' });
     }
-};
+}
 
 export const authRoles = {
     isSuperAdmin,
     isAdmin,
     isAuthenticated
-};
+}
