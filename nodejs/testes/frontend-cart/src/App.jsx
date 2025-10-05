8761e81f0840eca262b302de507c42990e61bc41
liliul <liliuestudo@gmail.com>
2025-10-05 15:03:22 -0300

    chore: reorganizando rotas em AppRoutes frontend-cart

--- nodejs/testes/frontend-cart/src/App.jsx
+++ nodejs/testes/frontend-cart/src/App.jsx
@@ -1,63 +1,12 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Register from './components/auth/register'
-import Login from './components/auth/login'
-
-import './App.css'
-import { TestandoContext } from './components/testandoContext'
-// import { TestandoDashboard } from './components/testandoContext/testandoDashboard'
-import { AuthRolesRoute } from './router/authRoles'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './router'
 import { AuthProvider } from './context/auth/authContext'
-import Layout from './layout'
-import { AdminDashboard } from './pages/dashboard/Administrations/admin'
-import { CreateProducts } from './components/products/createProducts'
-import { ProductsList } from './components/products/listProducts'
-import { ListUsers } from './components/users/list-users'
-import { DashboardSuperAdmin } from './pages/dashboard/Administrations/superAdmin'
-import { AddRoles } from './components/superAdmin/add-roles'
-import { AppProviders } from './context/providers'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AppProviders>
-            <Routes>
-            <Route path="/" element={<Layout />} >
-              <Route index element={<Register />} />
-
-              <Route path='/login' element={<Login /> } /> 
-              <Route path='/teste' element={<TestandoContext /> } />
-
-              <Route path='/products' element={
-                <AuthRolesRoute>
-                  <ProductsList />
-                </AuthRolesRoute>
-              } />
-
-              <Route path='/dashboard/admin' element={
-                <AuthRolesRoute roles={['admin', 'super-admin']}>
-                  {/* <TestandoDashboard /> */}
-                  <AdminDashboard />
-                  
-                    
-                </AuthRolesRoute>
-              }>
-                <Route path="create" element={<CreateProducts />} />
-                <Route path="list" element={<ProductsList />} />
-
-                <Route path="list-users" element={<ListUsers />} />
-              </Route> 
-
-              <Route path='/dashboard/super-admin' element={
-                <AuthRolesRoute roles={['super-admin']}>
-                    <DashboardSuperAdmin /> 
-                </AuthRolesRoute>
-              }>
-                <Route path='add-roles' element={<AddRoles />} />
-              </Route>
-            </Route>
-          </Routes>
-        </AppProviders>
+        <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
   )
---
+++ nodejs/testes/frontend-cart/src/router/index.jsx
@@ -0,0 +1,58 @@
+import { Routes, Route } from 'react-router-dom'
+import Register from '../components/auth/register'
+import Login from '../components/auth/login'
+
+import { TestandoContext } from '../components/testandoContext'
+// import { TestandoDashboard } from './components/testandoContext/testandoDashboard'
+import { AuthRolesRoute } from './authRoles'
+import Layout from '../layout'
+import { AdminDashboard } from '../pages/dashboard/Administrations/admin'
+import { CreateProducts } from '../components/products/createProducts'
+import { ProductsList } from '../components/products/listProducts'
+import { ListUsers } from '../components/users/list-users'
+import { DashboardSuperAdmin } from '../pages/dashboard/Administrations/superAdmin'
+import { AddRoles } from '../components/superAdmin/add-roles'
+import { AppProviders } from '../context/providers'
+
+export function AppRoutes() {
+    return (
+        <AppProviders>
+            <Routes>
+            <Route path="/" element={<Layout />} >
+              <Route index element={<Register />} />
+
+              <Route path='/login' element={<Login /> } /> 
+              <Route path='/teste' element={<TestandoContext /> } />
+
+              <Route path='/products' element={
+                <AuthRolesRoute>
+                  <ProductsList />
+                </AuthRolesRoute>
+              } />
+
+              <Route path='/dashboard/admin' element={
+                <AuthRolesRoute roles={['admin', 'super-admin']}>
+                  {/* <TestandoDashboard /> */}
+                  <AdminDashboard />
+                  
+                    
+                </AuthRolesRoute>
+              }>
+                <Route path="create" element={<CreateProducts />} />
+                <Route path="list" element={<ProductsList />} />
+
+                <Route path="list-users" element={<ListUsers />} />
+              </Route> 
+
+              <Route path='/dashboard/super-admin' element={
+                <AuthRolesRoute roles={['super-admin']}>
+                    <DashboardSuperAdmin /> 
+                </AuthRolesRoute>
+              }>
+                <Route path='add-roles' element={<AddRoles />} />
+              </Route>
+            </Route>
+          </Routes>
+        </AppProviders>
+    )
+}
