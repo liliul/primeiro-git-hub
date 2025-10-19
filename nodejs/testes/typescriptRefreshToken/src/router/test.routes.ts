982e47812ca566b586795bb3d66dc1be433bcd22
liliul <liliuestudo@gmail.com>
2025-10-18 22:00:12 -0300

    create: rota private test typescriptRefreshToken

--- nodejs/testes/typescriptRefreshToken/src/router/test.routes.ts
+++ nodejs/testes/typescriptRefreshToken/src/router/test.routes.ts
@@ -19,7 +19,7 @@ router.get('/home', authenticateToken, UsuarioRoles('user', 'moderador', 'admin'
 
     const userId = req.user.id;
     const userEmail = req.user.email;
-    console.log(userId);
+    console.log('id home',userId);
     
     const result = await pool.query('SELECT name FROM users WHERE id = $1', [userId]);
     const userName = result.rows[0].name;
@@ -38,6 +38,26 @@ router.put('/nova-role/:id',
     UsuarioRoles('superAdmin'),
     rolesController 
 )
+
+router.get('/private', authenticateToken, UsuarioRoles('admin', 'superAdmin'), async (req: Request, res: Response) => {
+    if (!req.user?.role) {
+        console.log('role private', req.user?.role);
+        
+        return res.status(401).json({ error: "Usuário não autenticado após middleware." });
+    }
+
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [req.user?.id]);
+    const userName = result.rows[0];
+
+    return res.json({ 
+        message: "Dados rota privada!",
+        userName: userName.name,
+        userEmail: userName.email,
+        userId: userName.id,
+        userRole: userName.roles,
+        dataHora: new Date()
+    });
+})
 
 
 export default router
