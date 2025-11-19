c4acf93623f7c4f9c3f3e4cf831231eda828ee42
liliul <liliuestudo@gmail.com>
2025-11-19 14:29:33 -0300

    update: callback desafio typescript

--- typescript/desafios/callback.js
+++ typescript/desafios/callback.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function CallBack(_a) {
     var nome = _a.nome, callback = _a.callback;
     var msg = 'Ola ' + nome;
@@ -10,3 +21,48 @@ function mensagem(mensagem) {
     return console.log(msgUpperCase);
 }
 CallBack({ nome: 'Naruto Uzumaki', callback: mensagem });
+function QualAnime(_a, chucksJson) {
+    var id = _a.id, nome = _a.nome, anime = _a.anime, author = _a.author, origem = _a.origem, description = _a.description, isperadoEm = _a.isperadoEm;
+    var construirJson = {
+        id: id,
+        nome: nome,
+        anime: anime,
+        author: author,
+        origem: origem,
+        description: description,
+        isperadoEm: isperadoEm
+    };
+    var primeirasInfos = {
+        id: id,
+        nome: nome,
+        anime: anime,
+        author: author
+    };
+    var segundaInfos = {
+        origem: origem,
+        description: description,
+        isperadoEm: isperadoEm
+    };
+    chucksJson(construirJson, primeirasInfos, segundaInfos);
+}
+function chucksJson(original, primeiro, segunda) {
+    console.log('original: ', original);
+    console.log('primeiro chuck: ', primeiro);
+    console.log('segundo chuck: ', segunda);
+    console.log('juntando primeiro e segunda: ', __assign(__assign({}, primeiro), segunda));
+}
+QualAnime({
+    id: 1,
+    nome: 'Naruto tokyo tv',
+    anime: 'naruto classico',
+    author: 'Masashi kishimoto',
+    origem: 'JP',
+    description: 'Naruto classico é um anime japones feito pelo mangaka "Masashi kishimoto"',
+    isperadoEm: [
+        'dragon ball',
+        'dragon ball z',
+        'super 11',
+        'cavaleiros do zodioco',
+        'hiyo hiyo oh'
+    ]
+}, chucksJson);
--- typescript/desafios/callback.ts
+++ typescript/desafios/callback.ts
@@ -18,3 +18,81 @@ function mensagem(mensagem: string): void {
 }
 
 CallBack({nome: 'Naruto Uzumaki', callback: mensagem})
+
+// -------------------------------------------------------- //
+
+interface AnimeCallback {
+    id: number
+    nome: string 
+    anime: string 
+    author: string 
+    origem: string 
+    description: string 
+    isperadoEm: string[] | string
+}
+
+// interface FunctionCallbacks {
+//     chucksJson: (original: object | string[], primeiro: object | string[], segunda: object | string[]) => void 
+// }
+type FunctionCallbacks = (
+    original: object | string[],
+    primeiro: object | string[],
+    segunda: object | string[]
+) => void;
+
+function QualAnime(
+    {id, nome, anime, author, origem, description, isperadoEm}: AnimeCallback, 
+    chucksJson: FunctionCallbacks
+): void {
+
+
+    const construirJson: object = {
+        id: id,
+        nome: nome,
+        anime: anime,
+        author: author,
+        origem: origem,
+        description: description,
+        isperadoEm: isperadoEm
+    }
+
+    const primeirasInfos: Omit<AnimeCallback, 'origem' | 'description' | 'isperadoEm'> = {
+        id: id,
+        nome: nome,
+        anime: anime,
+        author: author
+    }
+    const segundaInfos: Omit<AnimeCallback, 'id' | 'nome' | 'anime' | 'author'> = {
+        origem: origem,
+        description: description,
+        isperadoEm: isperadoEm
+    }
+
+    chucksJson(construirJson, primeirasInfos, segundaInfos)
+}
+
+function chucksJson(original: object | string[], primeiro: object | string[], segunda: object | string[]): void {
+    console.log('original: ', original);
+    console.log('primeiro chuck: ', primeiro);
+    console.log('segundo chuck: ', segunda);  
+    console.log('juntando primeiro e segunda: ', {...primeiro, ...segunda});
+    
+}
+
+QualAnime({
+    id: 1,
+    nome: 'Naruto tokyo tv',
+    anime: 'naruto classico',
+    author: 'Masashi kishimoto',
+    origem: 'JP',
+    description: 'Naruto classico é um anime japones feito pelo mangaka "Masashi kishimoto"',
+    isperadoEm: [
+        'dragon ball',
+        'dragon ball z',
+        'super 11',
+        'cavaleiros do zodioco',
+        'hiyo hiyo oh'
+    ]
+},
+chucksJson
+)
