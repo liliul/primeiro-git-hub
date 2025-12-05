bcf713301a57142f2123741ddf717547d98f841e
liliul <liliuestudo@gmail.com>
2025-12-05 14:50:57 -0300

    chore: reorganizando frontend para pasta public youtubeData

--- nodejs/testes/youtubeData/src/index.js
+++ nodejs/testes/youtubeData/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 }))
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "src/views")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(routerHealth)
 app.use(routerAccount)
--- nodejs/testes/youtubeData/src/router/logger.js
+++ nodejs/testes/youtubeData/src/router/logger.js
@@ -6,7 +6,7 @@ const routerLogger = express.Router();
 const __dirname = path.resolve();
 
 routerLogger.get("/home", authRequirida, (req, res) => {
-  res.sendFile(path.join(__dirname, "src/views/home.html"));
+  res.sendFile(path.join(__dirname, "public/views/home.html"));
 });
 
 export default routerLogger;
