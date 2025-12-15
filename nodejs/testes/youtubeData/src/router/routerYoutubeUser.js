3a74085cac816bb8b79951caf6504335e888f4e5
liliul <liliuestudo@gmail.com>
2025-12-15 08:42:17 -0300

    feat: playlists do usuario youtubeData

--- nodejs/testes/youtubeData/src/router/routerYoutubeUser.js
+++ nodejs/testes/youtubeData/src/router/routerYoutubeUser.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getMyChannel } from '../youtubeUser/controller.js'
+import { getMyChannel, getMyPlayList } from '../youtubeUser/controller.js'
 import authRequirida from '../middleware/autenticandoRotas.js'
 
 const routerYoutubeUser = express.Router()
 
 routerYoutubeUser.get('/ytuser', authRequirida, getMyChannel)
+routerYoutubeUser.get('/ytlist', authRequirida, getMyPlayList)
 
 export default routerYoutubeUser
--- nodejs/testes/youtubeData/src/youtubeUser/controller.js
+++ nodejs/testes/youtubeData/src/youtubeUser/controller.js
@@ -26,3 +26,69 @@ export async function getMyChannel(req, res) {
     res.status(500).json({ error: "Erro ao buscar canal" });
   }
 }
+
+export async function getMyPlayList(req, res) {
+  try {
+    const googleId = req.user.sub; 
+    const accessToken = await getValidGoogleToken(googleId)
+
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/playlists",
+      {
+        params: {
+          part: "snippet,contentDetails",
+          mine: true,
+          maxResults: 25
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+
+    if (!response.data.items || response.data.items.length === 0) {
+      return res.json([]);
+    }
+
+    const playListItems = await Promise.all(response.data.items.map(async (i) => {
+      console.log(i);
+      const videos = await getPlaylistVideos(accessToken, i.id)
+
+      return {
+        id: i.id,
+        title: i.snippet.title,
+        thumbnailList: i.snippet.thumbnails?.default?.url,
+        videos: videos.items.map((video) => ({
+          videoId: video.snippet?.resourceId?.videoId,
+          title: video.snippet.title,
+          url: `https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`,
+          thumbnail: video.snippet.thumbnails?.medium?.url
+        }))
+      }
+
+    }))
+
+    res.json(playListItems);
+  } catch (error) {
+    console.error('Erro no getMyPlayList: ', error);
+    res.status(500).json({ error: "Erro ao buscar playlists" }); 
+  }
+}
+
+async function getPlaylistVideos(accessToken, playlistId) {
+  const response = await axios.get(
+    "https://www.googleapis.com/youtube/v3/playlistItems",
+    {
+      params: {
+        part: "snippet,contentDetails",
+        playlistId,
+        maxResults: 25
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  return response.data;
+}
