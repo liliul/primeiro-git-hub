a365f257fab9edcd99eed0b17dcb783933901941
liliul <liliuestudo@gmail.com>
2026-01-20 16:01:03 -0300

    fix: routerError e pasta errors em public youtubeData

--- nodejs/testes/youtubeData/src/router/routerError.js
+++ nodejs/testes/youtubeData/src/router/routerError.js
@@ -1,18 +1,20 @@
 import express from 'express'
+import path from "path"
 
 const routerError = express.Router()
+const __dirname = path.resolve()
+
+routerError.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).sendFile(
+    path.join(__dirname, "public/views/errors/500.html")
+  )
+})
 
 routerError.use((req, res) => {
   res.status(404).sendFile(
-    path.join(__dirname, "views", "errors", "404.html")
-  );
-});
-
-routerError.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).sendFile(
-    path.join(__dirname, "views", "errors", "500.html")
-  );
-});
+    path.join(__dirname, "public/views/errors/404.html")
+  )
+})
 
 export default routerError
