341d5a1ceb049c8fa8dac682a0eb09acfdec08fb
liliul <liliuestudo@gmail.com>
2026-02-27 21:55:02 -0300

    add: publicRateLimit nas rotas esqueci e restaurar senha plano

--- nodejs/testes/plano/backend/src/modules/auth/resetPassword/routes.js
+++ nodejs/testes/plano/backend/src/modules/auth/resetPassword/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { pool } from "../../../database/postgres.js";
+import { publicRateLimit } from "../../../middlewares/rateLimit/rateLimit.js";
 import EsqueciSenhaController from "./esqueciSenhaController.js";
 import RestaurarSenhaController from "./restaurarSenhaControlle.js";
 
@@ -10,22 +11,30 @@ const restaurarSenha = new RestaurarSenhaController(pool);
 
 resetPassword.post(
 	"/esqueci-senha",
+	publicRateLimit("esqueci senha"),
 	esqueciSenha.forgotPassword.bind(esqueciSenha),
 );
+
 resetPassword.post(
 	"/restaurar-senha",
+	publicRateLimit("restaurar senha"),
 	restaurarSenha.resetPassword.bind(restaurarSenha),
 );
-resetPassword.get("/restaurar-senha", (req, res) => {
-	const { token } = req.query;
 
-	res.send(`
+resetPassword.get(
+	"/restaurar-senha",
+	publicRateLimit("alterando senha"),
+	(req, res) => {
+		const { token } = req.query;
+
+		res.send(`
     <form method="POST" action="/auth/restaurar-senha">
       <input type="hidden" name="token" value="${token}" />
       <input type="password" name="newPassword" placeholder="Nova senha" />
       <button type="submit">Redefinir</button>
     </form>
   `);
-});
+	},
+);
 
 export default resetPassword;
