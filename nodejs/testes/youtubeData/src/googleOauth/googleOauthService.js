42d007db06b5935a6719a38d86fefe9b1a9d8aeb
liliul <liliuestudo@gmail.com>
2026-04-17 09:48:07 -0300

    refactor: repository em googleOauthService youtubeData

--- nodejs/testes/youtubeData/src/googleOauth/googleOauthService.js
+++ nodejs/testes/youtubeData/src/googleOauth/googleOauthService.js
@@ -1,16 +1,16 @@
 import { AppError } from "../errors/AppError.js"
 
 class GoogleOauthService {
-    constructor(googleOauthAdapter, repository) {
+    constructor(googleOauthAdapter, googleOauthRepository) {
         this.googleOauthAdapter = googleOauthAdapter
-        this.repository = repository
+        this.googleOauthRepository = googleOauthRepository
     }
 
     googleOauthService() {
         const googleOauth2 = this.googleOauthAdapter.construindoUrlGoogleOaut2()            
 
         if(!googleOauth2) {
-            throw new AppError("Error", 401)
+            throw new AppError("Erro na construção da url do google oauth2", 401)
         }
 
         return googleOauth2
@@ -30,7 +30,7 @@ class GoogleOauthService {
         const googleUser = await this.googleOauthAdapter.verifacandoIDToken(id_token)
         
         const expiresAt = new Date(Date.now() + expires_in * 1000)
-        await this.repository.googleOauthTokens({
+        await this.googleOauthRepository.googleOauthTokens({
             sub: googleUser.sub,
             email: googleUser.email,
             accessToken: access_token,
