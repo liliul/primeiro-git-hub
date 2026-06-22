864e5e34e871a63517d7e496dfec200fd6ed81a6
liliul <liliuestudo@gmail.com>
2026-06-22 00:56:24 -0300

    add: redis meService no googleOauth youtubeData

--- nodejs/testes/youtubeData/src/googleOauth/meService.js
+++ nodejs/testes/youtubeData/src/googleOauth/meService.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { AppError } from "../errors/AppError.js"
+import { redis } from '../db/redis.js'
 
 class MeService {
     constructor(googleOauthRepository) {
@@ -11,10 +12,10 @@ class MeService {
     async me(req, res, next) {
        try {
         const accessToken = req.cookies.accessToken
-        
         if (!accessToken) {
             throw new AppError('AccessToken é obrigatorio.', 401)
         }
+
         const payload = jwt.verify(
             accessToken,
             process.env.JWT_SECRET,
@@ -23,6 +24,15 @@ class MeService {
                 audience: "my-video-you-web",
             }
         )
+
+        const cacheKey = `youtube:meservice:googleoauth:${payload.sub}`
+        const cached = await redis.get(cacheKey)
+        if (cached) {
+            return res.status(201).json({
+                source: 'redis',
+                user: JSON.parse(cached)
+            })
+        }
 
         const googleUserInfos = await this.googleOauthRepository.bucarGoogleIdBySub(payload.sub)
         if (!googleUserInfos) {
@@ -36,7 +46,11 @@ class MeService {
             picture: googleUserInfos.picture
         }
 
-        res.status(200).json({ user: user })
+        await redis.set(cacheKey, JSON.stringify(user), {
+            EX: 1200
+        })
+
+        res.status(200).json({ source: 'postgres', user: user })
        } catch (error) {
         next(error)
        }
