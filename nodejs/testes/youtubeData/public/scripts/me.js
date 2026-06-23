cb1bfcc90b3bb42a59e497d6c493e8a1d21cf869
liliul <liliuestudo@gmail.com>
2026-06-22 21:43:25 -0300

    update: me script frontend youtubeData

--- nodejs/testes/youtubeData/public/scripts/me.js
+++ nodejs/testes/youtubeData/public/scripts/me.js
@@ -1,28 +1,83 @@
-export async function Me() {
-    const req = await fetch("http://localhost:3001/v3/me", {
-        method: "GET",
-        credentials: "include", 
-    })
-    const res = await req.json()
+async function refreshToken() {
+    const response = await fetch(
+        "http://localhost:3001/auth/refresh",
+        {
+            method: "POST",
+            credentials: "include"
+        }
+    )
+    console.log('chammando refreshToken: ', response);
+    
+    return response.ok
+}
 
-    return res
-} 
+export async function authFetch(url, options = {}) {
+    let response = await fetch(url, {
+        ...options,
+        credentials: "include"
+    })
+    
+    if (response.status === 401) {
+        const refreshed = await refreshToken()
+    
+        if (!refreshed) {
+            window.location.href = "/"
+            return null
+        }
+
+        response = await fetch(url, {
+            ...options,
+            credentials: "include"
+        })
+    }
+
+    return response
+}  
+
+export async function Me() {
+    const response = await authFetch(
+        "http://localhost:3001/auth/ggme"
+    )
+    
+    if (!response) {
+        return null
+    }
+
+    return response.json()
+}
 
 export async function LayoutMe() {
     const me = await Me()
-    console.log(me)
+    console.log('me', me);
     
-    const section = document.createElement('section')
-    section.setAttribute('data-id-google', me.user.sub)
-    const img = document.createElement('img')
+    if (!me) {
+        window.location.href = "/"
+        return
+    }
+
+    const section = document.createElement("section")
+
+    section.setAttribute(
+        "data-id-google",
+        me.user.sub
+    )
+
+    const img = document.createElement("img")
     img.src = me.user.picture
 
-    const nomeEmail = document.createElement('div')
-    nomeEmail.innerHTML = `<strong>${me.user.name}</strong><br><p>${me.user.email}</p>`
-    
+    const nomeEmail = document.createElement("div")
+
+    nomeEmail.innerHTML = `
+        <strong>${me.user.name}</strong>
+        <br>
+        <p>${me.user.email}</p>
+    `
+
     section.append(img, nomeEmail)
 
-    document.getElementById('auth').append(section)
+    document
+        .getElementById("auth")
+        .append(section)
 }
 
 LayoutMe()
