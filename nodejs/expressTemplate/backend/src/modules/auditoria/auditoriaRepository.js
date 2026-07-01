68d0bdbe967c30fe872879c00b28a411d6c7df56
liliul <liliuestudo@gmail.com>
2026-07-01 17:20:24 -0300

    refactor: auditoria repository backend expresstemplate

---
+++ nodejs/expressTemplate/backend/src/modules/auditoria/auditoriaRepository.js
@@ -0,0 +1,37 @@
+class AuditoriaRepository {
+	constructor(pool) {
+		this.pool = pool;
+	}
+
+	async criandoAuditoriaAuth({
+		userId,
+		email,
+		action,
+		ip,
+		userAgent,
+		metadata,
+	}) {
+		await this.pool.query(
+			`
+      INSERT INTO auth_audit_logs (
+        user_id,
+        email,
+        action,
+        ip,
+        user_agent,
+		metadata
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      `,
+			[
+				userId || null,
+				email || null,
+				action,
+				ip,
+				userAgent,
+				metadata ? JSON.stringify(metadata) : null,
+			],
+		);
+	}
+}
+
+export default AuditoriaRepository;
--- nodejs/expressTemplate/backend/src/modules/auditoria/auditoriaService.js
+++ nodejs/expressTemplate/backend/src/modules/auditoria/auditoriaService.js
@@ -1,28 +1,14 @@
+import AuditoriaRepository from "./auditoriaRepository.js";
+
 class AuditoriaService {
 	constructor(pool) {
 		this.pool = pool;
+
+		this.auditoriaRepository = new AuditoriaRepository(this.pool);
 	}
-	async log({ userId, email, action, ip, userAgent, metadata }) {
-		await this.pool.query(
-			`
-      INSERT INTO auth_audit_logs (
-        user_id,
-        email,
-        action,
-        ip,
-        user_agent,
-		metadata
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      `,
-			[
-				userId || null,
-				email || null,
-				action,
-				ip,
-				userAgent,
-				metadata ? JSON.stringify(metadata) : null,
-			],
-		);
+
+	async log(data) {
+		await this.auditoriaRepository.criandoAuditoriaAuth(data);
 	}
 }
 
