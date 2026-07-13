ad301c235bcad814bc990ecf7ca20058fb3a051a
liliul <liliuestudo@gmail.com>
2026-07-13 16:40:21 -0300

    refactor: Hashservice bcrypt para argon2 youtubeData

--- nodejs/testes/youtubeData/src/admin/utils/utils.js
+++ nodejs/testes/youtubeData/src/admin/utils/utils.js
@@ -1,13 +1,39 @@
-import bcrypt from 'bcrypt'
+import argon2 from 'argon2'
+import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export class HashService {
-  async compare(plain, hashed) {
-    return bcrypt.compare(plain, hashed)
+  constructor() {
+    this.PEPPER = process.env.PASSWORD_PEPPER
+    
+    if (!this.PEPPER) {
+			throw new Error("PASSWORD_PEPPER pode estar ausente na .env");
+		}
   }
 
-  async hash(plain, rounds = 10) {
-    return bcrypt.hash(plain, rounds)
+  applyPepper(password) {
+    return crypto.createHmac('sha256', this.PEPPER).update(password).digest('hex')
+  }
+
+  async compare(password, hash) {
+    const peppered = this.applyPepper(password)
+
+    return argon2.verify(hash, peppered)
+  }
+
+  async hash(password) {
+    const peppered = this.applyPepper(password)
+
+    const hash = await argon2.hash(peppered, {
+      type: argon2.argon2id, 
+      memoryCost: 19456,   
+      timeCost: 2,
+      parallelism: 1,
+    })
+
+    return hash
   }
 }
 
