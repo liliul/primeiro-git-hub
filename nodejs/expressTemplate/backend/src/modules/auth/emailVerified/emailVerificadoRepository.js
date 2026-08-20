377833cf93a150ebe1b7791ec0885f51e6af3133
liliul <liliuestudo@gmail.com>
2026-08-20 15:43:06 -0300

    create: email verificado repository expressTemplate

---
+++ nodejs/expressTemplate/backend/src/modules/auth/emailVerified/emailVerificadoRepository.js
@@ -0,0 +1,27 @@
+class EmailVerificadoRepository {
+	constructor(pool) {
+		this.pool = pool;
+	}
+
+	async createEmailVerifiedTokens(user_id, tokenHash, expireAt) {
+		await this.pool.query(
+			`
+			insert into email_verification_tokens (user_id, token_hash, expires_at) values ($1, $2, $3)
+		`,
+			[user_id, tokenHash, expireAt],
+		);
+	}
+
+	async updateEmailVerifiedFalse(user_id) {
+		await this.pool.query(
+			`
+            UPDATE users
+            SET email_verified = false
+            WHERE id = $1    
+        `,
+			[user_id],
+		);
+	}
+}
+
+export default EmailVerificadoRepository;
