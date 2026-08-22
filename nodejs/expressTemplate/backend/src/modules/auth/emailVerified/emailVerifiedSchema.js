bed445fcef6ef479f9dd64cc8c5941f1a9dae844
liliul <liliuestudo@gmail.com>
2026-08-21 22:56:55 -0300

    fix: zod email object expressTemplate

--- nodejs/expressTemplate/backend/src/modules/auth/emailVerified/emailVerifiedSchema.js
+++ nodejs/expressTemplate/backend/src/modules/auth/emailVerified/emailVerifiedSchema.js
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-export const emailUserSchema = z.object({
-	email: z.string().email("Email inválido"),
-});
+export const emailUserSchema =  z.string().email("Email inválido");
 
 export const userIdSchema = z.string().uuid();
