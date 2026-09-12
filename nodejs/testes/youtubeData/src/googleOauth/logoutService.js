ebeaf2500c76012fa4fbc1c87acd1ddc74313e49
liliul <liliuestudo@gmail.com>
2026-09-12 19:05:01 -0300

    refactor: logout googleOauth idempotente youtubeData

--- nodejs/testes/youtubeData/src/googleOauth/logoutService.js
+++ nodejs/testes/youtubeData/src/googleOauth/logoutService.js
@@ -1,4 +1,3 @@
-import { AppError } from "../errors/AppError.js"
 import { cookies } from "./cookies.js"
 
 class LogoutService {
@@ -12,20 +11,16 @@ class LogoutService {
         try {
             const refreshToken = req.cookies.refreshToken
 
-            if (!refreshToken) {
-                throw new AppError( "Refresh token obrigatório", 401)
+            if (refreshToken) {
+                const buscandoRefreshToken = await this.googleOauthRepository.buscandoRefreshTokenByToken(refreshToken)
+                
+                if (buscandoRefreshToken) {
+                    await this.googleOauthRepository.deletarRefreshTokenById(buscandoRefreshToken.id)
+                    await this.googleOauthRepository.deletarGoogleOauthTokensBySub(buscandoRefreshToken.google_id)
+                }
             }
-
-            const buscandoRefreshToken = await this.googleOauthRepository.buscandoRefreshTokenByToken(refreshToken)
-            if (!buscandoRefreshToken) {
-                throw new AppError('Error: busca pelo refresh token não existe.', 401)
-            }
-            
-            await this.googleOauthRepository.deletarRefreshTokenById(buscandoRefreshToken.id)
-            await this.googleOauthRepository.deletarGoogleOauthTokensBySub(buscandoRefreshToken.google_id)
 
             res.clearCookie("accessToken", cookies.logoutAccessToken)
-
             res.clearCookie("refreshToken", cookies.logoutRefreshToken)
             
             res.status(204).send()
