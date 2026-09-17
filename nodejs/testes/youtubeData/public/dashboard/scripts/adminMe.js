8a74aa53ee37473d5f1c76124df33ab90af3124a
liliul <liliuestudo@gmail.com>
2026-09-16 21:09:12 -0300

    refactor: reorganizando views dashboard adminMe youtubeData

--- nodejs/testes/youtubeData/public/dashboard/scripts/adminMe.js
+++ nodejs/testes/youtubeData/public/dashboard/scripts/adminMe.js
@@ -3,14 +3,71 @@ import { authFetch } from "../core/authFetchAdmin.js";
 async function me() {
     const me = await authFetch('/admin/me')
     const response = await me.json()
+
+    if (!response) {
+        return document.getElementById('me').textContent = 'Erro na resposta do ME.'
+    }
+    
     console.log(response);
     
     document.getElementById('me').innerHTML = `
-        <p>${response.me.id}</p>
-        <p>${response.me.name}</p>
-        <p>${response.me.email}</p>
-        <p>${response.me.role}</p>
-        <p>${response.me.criado_em}</p>
+        <ul>
+    
+            <li class="li-infos">
+                <span class="icon">🏠</span>
+                <span class="text">
+                    ${response.me.id}
+                </span>    
+            </li>
+
+            <li class="li-infos">
+                <span class="icon">▶️</span>
+                <span class="text">
+                    ${response.me.name}
+                </span>
+            </li>
+                
+            <li class="li-infos">
+                <span class="icon">📺</span>
+                <span class="text">
+                    ${response.me.email}
+                </span>
+            </li>
+
+            <hr class="separator">
+
+            <li class="li-infos">
+                <span class="icon">📚</span>
+                <span class="text">
+                    ${response.me.role}
+                </span>
+            </li>
+
+            <li class="li-infos">
+                <span class="icon">🕘</span>
+                <span class="text">
+                    ${response.me.criado_em}
+                </span>
+            </li>
+
+            <li class="li-infos">
+                <span class="icon">🎬</span>
+                <span class="text">Seus vídeos</span>
+            </li>
+
+            <li class="li-infos">
+                <span class="icon">⏱️</span>
+                <span class="text">Assistir mais tarde</span>
+            </li>
+
+            <hr class="separator">
+
+            <li class="li-infos">
+                <span class="icon">👍</span>
+                <span class="text">Vídeos marcados</span>
+            </li>
+
+        </ul>
     `
 }
 me()
